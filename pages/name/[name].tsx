510074118dd5b6a77b8cd486fb004cdca707c8fb
Federico Rodriguez <federico.rodriguez@intive.com>
2022-08-04 19:33:26 -0300

    refactor function

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -6,7 +6,7 @@ import confetti from "canvas-confetti";
 import { Layout } from "../../components/layouts";
 import { pokeApi } from "../../api";
 import { Pokemon, PokemonListResponse } from "../../interfaces";
-import { localFavorites } from "../../utils";
+import { getPokemonInfo, localFavorites } from "../../utils";
 
 interface Props {
   pokemon: Pokemon;
@@ -122,17 +122,10 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { name } = params as { name: string };
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${name}`);
-
-  const pokemon = {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites,
-  };
 
   return {
     props: {
-      pokemon: pokemon,
+      pokemon: await getPokemonInfo(name),
     },
   };
 };
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -6,7 +6,7 @@ import confetti from "canvas-confetti";
 import { Layout } from "../../components/layouts";
 import { pokeApi } from "../../api";
 import { Pokemon } from "../../interfaces";
-import { localFavorites } from "../../utils";
+import { getPokemonInfo, localFavorites } from "../../utils";
 
 interface Props {
   pokemon: Pokemon;
@@ -121,17 +121,10 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`);
-
-  const pokemon = {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites,
-  };
 
   return {
     props: {
-      pokemon: pokemon,
+      pokemon: await getPokemonInfo(id),
     },
   };
 };
